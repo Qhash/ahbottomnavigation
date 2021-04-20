f48dae8bfa469d324aca30b28ed5affa49f231d2
mrqhash <45790484+mrqhash@users.noreply.github.com>
2021-04-21 02:05:03 +0400

    Update AHBottomNavigation.java
    Max items = 6

--- ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigation.java
+++ ahbottomnavigation/src/main/java/com/aurelhubert/ahbottomnavigation/AHBottomNavigation.java
@@ -67,7 +67,7 @@ public class AHBottomNavigation extends FrameLayout {
 	private static String TAG = "AHBottomNavigation";
     private static final String EXCEPTION_INDEX_OUT_OF_BOUNDS = "The position (%d) is out of bounds of the items (%d elements)";
 	private static final int MIN_ITEMS = 3;
-	private static final int MAX_ITEMS = 5;
+	private static final int MAX_ITEMS = 6;
 
 	// Listener
 	private OnTabSelectedListener tabSelectedListener;
